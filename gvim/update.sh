69a4e22fa94619639aacc74eeaba451557ec36ae
anekos <anekos@snca.net>
2015-04-23 13:22:43 +0900

    Add updater

---
+++ gvim/update.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+cd src/vim-repo
+hg pull -u
+tag=$(hg tags | asort -k2 -o 1 -rn | grep '^v' | head -1 | tr '-' '.' | sed 's/^v//')
+cd - > /dev/null
+
+topvar=$(echo "$tag" | awk -F. 'BEGIN{OFS="."} {print $1,$2}')
+patchlevel=$(echo "$tag" | awk -F. 'BEGIN{OFS="."} {print $3}')
+
+sed -i "s/^_topvar=.*/_topvar=$topvar/" PKGBUILD
+sed -i "s/^_patchlevel=.*/_patchlevel=$patchlevel/" PKGBUILD
+
+
+# 変更があるか？
+changed=$(git diff | wc -l)
+if [ "$changed" -eq 0 ]
+then
+  echo -e '\e[41mUp to date\e[0m'
+  exit 0
+fi
+
+
+echo "Current version: $topvar.$patchlevel"
+git diff --stat
+
+makepkg -f
+
+rm *.tar.xz
+
+sudo pacman -U gvim-python3-$topvar.$patchlevel-*.tar.xz vim-runtime-$topvar.$patchlevel*.tar.xz
+
+git commit PKGBUILD -m 'Automatic'
+
+echo 'push? (YES/no): '
+read q
+if [ "$q" = 'no' ]
+then
+  git push
+fi
