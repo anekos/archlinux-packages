15dc530a35c07a1d0af5815b19581bee5e574a75
anekos <anekos@snca.net>
2015-04-26 22:06:23 +0900

    Meow

--- gvim/update.sh
+++ gvim/update.sh
@@ -6,16 +6,17 @@ hg pull -u
 tag=$(hg tags | asort -k2 -o 1 -rn | grep '^v' | head -1 | tr '-' '.' | sed 's/^v//')
 cd - > /dev/null
 
-topvar=$(echo "$tag" | awk -F. 'BEGIN{OFS="."} {print $1,$2}')
+topver=$(echo "$tag" | awk -F. 'BEGIN{OFS="."} {print $1,$2}')
 patchlevel=$(echo "$tag" | awk -F. 'BEGIN{OFS="."} {print $3}')
 
-sed -i "s/^_topvar=.*/_topvar=$topvar/" PKGBUILD
+sed -i "s/^_topvar=.*/_topvar=$topver/" PKGBUILD
 sed -i "s/^_patchlevel=.*/_patchlevel=$patchlevel/" PKGBUILD
 
+sysver=$(LC_ALL=C pacman -Qi gvim-python3 | grep Version | sed 's/.*: //' | sed 's/-.*//')
 
 # 変更があるか？
 changed=$(git diff | wc -l)
-if [ "$changed" -eq 0 ]
+if [ "$changed" -eq 0 ] && [ "$sysver" = "$tag" ]
 then
   echo -e '\e[41mUp to date\e[0m'
   exit 0
